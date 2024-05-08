5217caddda929279b3dc15aed714aea0cff295de
antoniocraveiro <antonio.craveirojr@gmail.com>
2024-05-08 00:00:40 -0300

    Commit Inicial

---
+++ src/main/java/tech/study/dtoxdao/DtoxdaoApplication.java
@@ -0,0 +1,13 @@
+package tech.study.dtoxdao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoxdaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtoxdaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/study/dtoxdao/DtoxdaoApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.study.dtoxdao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtoxdaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
